e333f20ca19de0f39111f17235daa7f1912058bb
Ivan Gurev <gureff.al@yandex.ru>
2025-04-19 22:36:31 +0300

    Add doors open sound

--- Assets/Scripts/TriggerToDoors.cs
+++ Assets/Scripts/TriggerToDoors.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class TriggerToDoors : MonoBehaviour
 {
+    [SerializeField] private PlayerMove _playerMove;
     [SerializeField] private GameManager _gameManager;
     [SerializeField] private Animator _doorsAnimator;
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _audioClipWin;
+    [SerializeField] private AudioClip _audioClipLose;
     [SerializeField] private int _scoreNeeded;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<PlayerMove>() && _gameManager.PlayerScore >= _scoreNeeded)
+        if (!other.GetComponent<PlayerMove>()) return;
+
+        if (_gameManager.PlayerScore >= _scoreNeeded)
         {
             _doorsAnimator.enabled = true;
+            _audioSource.PlayOneShot(_audioClipWin);
+        }
+        else
+        {
+            _audioSource.PlayOneShot(_audioClipLose);
+            _playerMove.enabled = false;
         }
     }
 }
--- Assets/Scripts/TriggerToFlags.cs
+++ Assets/Scripts/TriggerToFlags.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class TriggerToFlags : MonoBehaviour
 {
     [SerializeField] private Animator _flagAnimator;
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _audioClip;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerMove>())
         {
             _flagAnimator.enabled = true;
+            _audioSource.PlayOneShot(_audioClip);
         }
     }
 }
