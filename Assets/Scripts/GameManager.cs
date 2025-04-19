bfc3e4808bf8c347d1aa18725608582fcd71bbe5
Ivan Gurev <gureff.al@yandex.ru>
2025-04-20 00:08:01 +0300

    Add player animations

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,9 +2,49 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameStart _gameStart;
+    [SerializeField] private Animator _playerAnimator;
+
     private int _playerScore = 40;
 
     public int PlayerScore => _playerScore;
+
+    private void Update()
+    { 
+        CheckPlayerState();
+    }
+
+    private void CheckPlayerState()
+    {
+        const int firstLevel = 60;
+        const int secondLevel = 90;
+
+        if (_gameStart.GameStarted)
+        {
+            if (_playerAnimator.GetBool("IsStarted") is false) _playerAnimator.SetBool("IsStarted", true);
+            print(_playerScore);
+            if (_playerScore >= firstLevel)
+            {
+                _playerAnimator.SetBool("IsLvlUpFirst", true);
+            }
+
+            if (_playerScore >= secondLevel)
+            {
+                _playerAnimator.SetBool("IsLvlUpSecond", true);
+            }
+
+            if (_playerScore < secondLevel)
+            {
+                _playerAnimator.SetBool("IsLvlUpSecond", false);
+            }
+
+            if (_playerScore < firstLevel)
+            {
+                _playerAnimator.SetBool("IsLvlUpFirst", false);
+            }
+        }
+
+    }
 
     public void IncreaseScore(int pointValue)
     {
--- Assets/Scripts/GameStart.cs
+++ Assets/Scripts/GameStart.cs
@@ -4,10 +4,15 @@ public class GameStart : MonoBehaviour
 {
     [SerializeField] private PlayerMove _playerMove;
 
+    private bool _gameStarted;
+
+    public bool GameStarted => _gameStarted;
+
     private void Update()
     {
         if (Input.touchCount > 0)
         {
+            _gameStarted = true;
             _playerMove.enabled = true;
             transform.GetComponent<GameStart>().enabled = false;
         }
